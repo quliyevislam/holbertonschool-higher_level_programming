4254cdadb1417ac84ea4c2362c2961fb12fcfd1b
Islam <9337@holbertonstudents.com>
2024-09-06 22:43:05 +0400

    Fixed checker error by changing `argument` to `arguments`

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
 
-    print("{} argument:".format(len(sys.argv) - 1))
+    print("{} arguments:".format(len(sys.argv) - 1))
 
     for i in range(1, len(sys.argv)):
         print("{}".format(f"{i}: {sys.argv[i]}"))
