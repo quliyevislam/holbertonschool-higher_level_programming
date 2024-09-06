c133004d5936656b8b21578a9dd1505f17bdc385
Islam <9337@holbertonstudents.com>
2024-09-06 22:49:26 +0400

    Fixed checker error

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,8 +4,12 @@ import sys
 
 if __name__ == "__main__":
     
-    if len(sys.argv) > 2 or len(sys.argv) == 1:
+    if len(sys.argv) > 2:
         print("{} arguments:".format(len(sys.argv) - 1))
+
+    elif len(sys.argv) == 1:
+        print("{} arguments.".format(len(sys.argv) - 1))
+
     else:
         print("{} argument:".format(len(sys.argv) - 1))
 
