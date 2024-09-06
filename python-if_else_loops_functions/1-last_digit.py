44140f53f46329437c7c4de2bd3c2b3aef3fd58a
Islam <9337@holbertonstudents.com>
2024-09-06 19:16:04 +0400

    Fix 1-last_digit.py pycodestyle error

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -9,5 +9,5 @@ if LastDigit == 0:
 elif LastDigit > 5:
     print(f"Last digit of {number} is {LastDigit} and is greater than 5")
 else:
-    print(f"Last digit of {number} is {LastDigit}", end = " ")
+    print(f"Last digit of {number} is {LastDigit}", end=" ")
     print("and is less than 6 and not 0")
