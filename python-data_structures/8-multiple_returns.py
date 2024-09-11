e66c4c7058e764916241fd969726d52d57ef11e2
Islam <9337@holbertonstudents.com>
2024-09-11 18:40:52 +0400

    Fixed bugs

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    length = len(sentence)
-    if (len == 0):
-        first = None
-    else:
-        first = sentence[0]
-    return (length, first)
+    return (len(sentence), None if len(sentence) == 0 else sentence[0])
