9d05684a5b02b55e189de901584a876af52a2c80
Islam <9337@holbertonstudents.com>
2024-09-14 23:17:24 +0400

    Fix checker error

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -4,5 +4,21 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except IndexError:
+    except ValueError:
         return False
+
+
+value = 89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = -89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = "School"
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
