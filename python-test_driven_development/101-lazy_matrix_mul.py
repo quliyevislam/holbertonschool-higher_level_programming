318f2e8dd5677a7f6853e67eebd6402d17b8abc0
Islam <9337@holbertonstudents.com>
2024-09-15 21:08:34 +0400

    -

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -20,7 +20,5 @@ def lazy_matrix_mul(m_a, m_b):
     
     try:
         return np.matmul(m_a, m_b)
-    except ValueError:
-        raise ValueError("m_a and m_b can't be multiplied")
     except TypeError:
         raise TypeError("Scalar operands are not allowed, use '*' instead")
