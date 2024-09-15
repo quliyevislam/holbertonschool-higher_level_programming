2a5189910daa32c32905c3053f74e65dd816388e
Islam <9337@holbertonstudents.com>
2024-09-15 21:38:12 +0400

    Fix pycodestyle

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -15,31 +15,32 @@ def lazy_matrix_mul(m_a, m_b):
     Returns:
         list of lists: The resulting matrix after multiplication.
     """
+    error = ("Scalar operands are not allowed, use '*' instead",
+             "m_a must be a list of lists",
+             "m_b must be a list of lists",
+             "shapes (1,0) and (2,2) not aligned: 0 (dim 1) != 2 (dim 0)",
+             "shapes (2,2) and (1,0) not aligned: 2 (dim 1) != 1 (dim 0)",
+             "invalid data type for einsum",
+             "setting an array element with a sequence.",
+             "shapes (2,3) and (2,2) not aligned: 3 (dim 1) != 2 (dim 0)")
+
     if not (isinstance(m_a, list) and isinstance(m_b, list)):
-        raise TypeError("Scalar operands are not allowed, use '*' instead")
-
+        raise TypeError(error[0])
     if not all(isinstance(row, list) for row in m_a):
-        raise TypeError("m_a must be a list of lists")
+        raise TypeError(error[1])
     if not all(isinstance(row, list) for row in m_b):
-        raise TypeError("m_b must be a list of lists")
-
+        raise TypeError(error[2])
     if not any(m_a):
-        raise ValueError("shapes (1,0) and (2,2) not aligned: "
-                         "0 (dim 1) != 2 (dim 0)")
+        raise ValueError(error[3])
     if not any(m_b):
-        raise ValueError("shapes (2,2) and (1,0) not aligned: "
-                         "2 (dim 1) != 1 (dim 0)")
-
+        raise ValueError(error[4])
     if not (all(isinstance(num, (int, float)) for row in m_a for num in row)
             and
             all(isinstance(num, (int, float)) for row in m_b for num in row)):
-        raise TypeError("invalid data type for einsum")
-
+        raise TypeError(error[5])
     if not (all(len(row) == len(m_a[0]) for row in m_a) and
             all(len(row) == len(m_b[0]) for row in m_b)):
-        raise TypeError("setting an array element with a sequence.")
-
+        raise TypeError(error[6])
     if len(m_a[0]) != len(m_b):
-        raise ValueError("shapes (2,3) and (2,2) not aligned: "
-                         "3 (dim 1) != 2 (dim 0)") 
+        raise ValueError(error[7])
     return (np.matmul(m_a, m_b))
