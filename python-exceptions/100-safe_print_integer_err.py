81ef1682529c0e444ee40c3ed00c381e2a62ccdb
Islam <9337@holbertonstudents.com>
2024-09-15 22:07:31 +0400

    -

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -3,10 +3,19 @@ import sys
 
 
 def safe_print_integer_err(value):
+    """
+    Prints an integer followed by a new line.
 
+    Args:
+        value: The value to print. Can be of any type.
+
+    Returns:
+        bool: True if the value is an integer and was printed successfully,
+              False otherwise, with a generic error message printed to stderr.
+    """
     try:
         print("{:d}".format(value))
         return True
-    except Exception:
-        print("Exception: {}".format(Exception), file=sys.stderr)
+    except (ValueError, TypeError):
+        print("Exception: Invalid integer format", file=sys.stderr)
         return False
