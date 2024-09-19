451c4aa4048b593b4aa90bdad5539921dd2d2ecb
Islam <9337@holbertonstudents.com>
2024-09-19 19:59:42 +0400

    Fix pycodestyle error

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """This class defines a square with a specified size."""
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
