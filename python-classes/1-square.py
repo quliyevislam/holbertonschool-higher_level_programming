43bd23c5e43fbf7cf74d786a8980150b81f34c1d
Islam <9337@holbertonstudents.com>
2024-09-19 19:16:39 +0400

    Add 1-square.py, 1-main.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines a simple class named Square."""
+
+
+class Square:
+    "This is the simplest class."
+    def __init__(self, size):
+        self._Square__size = size
