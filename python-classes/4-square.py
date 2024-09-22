6bc04cb096932d85fb30520767f720b6ec5f82ce
Islam <9337@holbertonstudents.com>
2024-09-22 11:42:15 +0400

    Add 4-square.py file

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -18,6 +18,14 @@ class Square:
     """
 
     def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
