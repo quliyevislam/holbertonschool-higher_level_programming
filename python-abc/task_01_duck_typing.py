f1285e2658a65c3d2b1fd6b2dad8e440a01df570
Islam <9337@holbertonstudents.com>
2024-09-29 17:04:02 +0400

    Trying to fix checker error

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -15,25 +15,25 @@ class Shape(ABC):
 
 class Circle(Shape):
     def __init__(self, radius):
-        self.radius = radius
+        self.__radius = radius
 
     def area(self):
-        return pi*self.radius**2
+        return pi*self.__radius**2
 
     def perimeter(self):
-        return 2*pi*self.radius
+        return 2*pi*self.__radius
 
 
 class Rectangle(Shape):
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     def area(self):
-        return self.height*self.width
+        return self.__height*self.__width
 
     def perimeter(self):
-        return 2*(self.width+self.height)
+        return 2*(self.__width+self.__height)
 
 
 def shape_info(Shape):
