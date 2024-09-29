e7f3845a7496d73b355a6b3fa14c57501de98e16
Islam <9337@holbertonstudents.com>
2024-09-29 13:55:00 +0400

    Fix syntax error in Rectangle class

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -13,7 +13,7 @@ class Rectangle(BaseGeometry):
         __height : int
     """
     def __init__(self, width, height):
-        integer_validator("width", width)
-        integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
