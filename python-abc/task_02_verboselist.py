f71ec363e6e8b1b16ef839f64456001fa9f70f32
Islam <9337@holbertonstudents.com>
2024-09-29 17:55:16 +0400

    Trying to fix checker error

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,20 +1,36 @@
 #!/usr/bin/python3
 
-
 class VerboseList(list):
+    """
+    A subclass of list that logs actions when modifying the list.
+    """
+
     def append(self, element):
+        """
+        Append an element to the list, printing a message before adding.
+        """
         print(f"Added {element} to the list.")
         super().append(element)
 
     def extend(self, element):
+        """
+        Extend the list with elements, printing a message before adding.
+        """
         print(f"Extended the list with {element} items.")
         super().extend(element)
 
     def remove(self, element):
-        print(f"Removed {element} from the list")
+        """
+        Remove the first occurrence of an element, printing a message.
+        """
+        print(f"Removed {element} from the list.")
         super().remove(element)
 
     def pop(self, index=None):
+        """
+        Pop an element at the specified index, printing a message.
+        If no index is provided, pop the last element.
+        """
         if index is None:
             index = len(self) - 1
         print(f"Popped {self[index]} from the list.")
