9f1542bf6e06fe133829a2c33781331a578e320e
Islam <9337@holbertonstudents.com>
2024-10-06 17:49:53 +0400

    Add module documentation

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This module contains:
+a script that adds all arguments to a Python list, and then save them to a file
+"""
 import sys
 import os
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
