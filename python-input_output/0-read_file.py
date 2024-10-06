79436c617cba38d6f772c5efa1c3c5f7c35162ff
Islam <9337@holbertonstudents.com>
2024-10-06 14:54:38 +0400

    Fix no newline char at the end of `print()`

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -11,4 +11,4 @@ def read_file(filename=""):
         filename : str
     """
     with open(filename, mode="r", encoding="UTF-8") as file:
-        print(file.read())
+        print(file.read(), end="")
