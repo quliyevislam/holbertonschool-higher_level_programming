76ea62d5a482b319a5ac75c198c602d83bc188dc
Islam <9337@holbertonstudents.com>
2024-10-06 18:21:30 +0400

    Remove script from 9-student module

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -25,14 +25,3 @@ class Student:
     def to_json(self):
         """Retrieves a dictionary representation of a Student"""
         return self.__dict__
-
-
-students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
-
-for student in students:
-    j_student = student.to_json()
-    print(type(j_student))
-    print(j_student['first_name'])
-    print(type(j_student['first_name']))
-    print(j_student['age'])
-    print(type(j_student['age']))
