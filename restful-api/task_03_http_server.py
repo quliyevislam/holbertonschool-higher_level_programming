742123cb72b23e170c45e703c1c4ac179d294b1d
Islam <9337@holbertonstudents.com>
2024-10-19 19:25:00 +0400

    Fix

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,39 +1,31 @@
 import json
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import http.server
 
-class SimpleAPIHandler(BaseHTTPRequestHandler):
+class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
             self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
+            self.send_header('Content-Type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b'Hello, this is a simple API!')
-        
+            self.wfile.write('Hello, this is a simple API!'.encode('utf-8'))
+
         elif self.path == '/data':
             self.send_response(200)
-            self.send_header('Content-type', 'application/json')
+            self.send_header('Content-Type', 'application/json')
             self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(data).encode('utf-8'))
-        
+
         elif self.path == '/status':
             self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
+            self.send_header('Content-Type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b'OK')
-        
+            self.wfile.write('OK'.encode('utf-8'))
+
         else:
             self.send_response(404)
-            self.send_header('Content-type', 'application/json')
+            self.send_header('Content-Type', 'text/plain')
             self.end_headers()
-            error_message = {"error": "Endpoint not found"}
-            self.wfile.write(b'404 Not Found\nEndpoint not found')
+            self.wfile.write('Endpoint not found'.encode('utf-8'))
 
-def run(server_class=HTTPServer, handler_class=SimpleAPIHandler, port=8000):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print(f'Serving on port {port}...')
-    httpd.serve_forever()
-
-if __name__ == "__main__":
-    run()
+http.server.HTTPServer(('', 8000), SimpleAPIHandler).serve_forever()
